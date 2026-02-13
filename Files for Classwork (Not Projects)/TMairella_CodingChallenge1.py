6699809e55d9da4c6ba9da8dd7a64422ac7c4d34
privatetom1208 <40084@ocvts.org>
2026-02-13 11:01:09 -0500

    codingchallenge push

---
+++ Files for Classwork (Not Projects)/TMairella_CodingChallenge1.py
@@ -0,0 +1,8 @@
+# Challenge 1 Starter Code
+name = input("What is your name? ")
+age = int(input("What is your age? "))
+height = float(input("What is your height in meters? "))
+
+print(type(name))
+print(type(age))
+print(type(height))
